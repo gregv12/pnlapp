f5f598f3fc41f3aab9165416ef2f6ea80bf1a73b
greg <gregphiggins@gmail.com>
2025-01-02 18:14:16 +0000

    upgrading config to latest version 0.1.53 for fluxtion server

--- src/main/java/com/telamin/app/pnl/BatchDtoDeserialiser.java
+++ src/main/java/com/telamin/app/pnl/BatchDtoDeserialiser.java
@@ -12,12 +12,11 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Log4j2
-public class BatchDtoDeserialiser<T> implements Function<List<String>, Object> {
+public class BatchDtoDeserialiser<T> implements Function<String, Object> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final Class<T> clazz;
@@ -30,29 +29,17 @@ public class BatchDtoDeserialiser<T> implements Function<List<String>, Object> {
     }
 
     @Override
-    public Object apply(List<String> lines) {
-        String lineInput = "NULL";
-        if (lines.size() == 0) {
+    public Object apply(String lineInput) {
+        if (lineInput == null || lineInput.isEmpty()) {
             return null;
         }
         try {
-            if (lines.size() == 1) {
-                lineInput= lines.get(0);
-                T SymbolDTO = objectMapper.readValue(lineInput, clazz);
-                log.debug("dto  - {}", SymbolDTO);
-                return SymbolDTO;
-            } else {
-                BatchDto<T> tradeBatchDTO = batchDtoSupplier.get();
-                for (String line : lines) {
-                    lineInput = line;
-                    T SymbolDTO = objectMapper.readValue(line, clazz);
-                    tradeBatchDTO.addBatchItem(SymbolDTO);
-                }
-                return tradeBatchDTO;
-            }
+            T SymbolDTO = objectMapper.readValue(lineInput, clazz);
+            log.debug("dto  - {}", SymbolDTO);
+            return SymbolDTO;
         } catch (JsonProcessingException e) {
             log.error(e);
-            logger.error("{} error processing input:'{}' error message:{}", getClass().getName(), lineInput, e.getMessage() );
+            logger.error("{} error processing input:'{}' error message:{}", getClass().getName(), lineInput, e.getMessage());
         }
         return null;
     }
